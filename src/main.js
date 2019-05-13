65547772af57defc9550059c44c3a25ab9d824de
Echi <914743765@qq.com>
2019-05-13 15:51:08 +0800

    搭建基础项目结构

---
+++ src/main.js
@@ -0,0 +1,30 @@
+// Require all the components that start with 'BaseXXX.vue'
+const components = require.context('../pakeages', true, /[a-z0-9]+\.vue$/);
+
+export function install(Vue, options = {}) {
+  // To extract the component name
+  const nameReg = /([a-z0-9]+)\./i;
+  // Registration
+  components.keys().forEach((key) => {
+    const name = key.match(nameReg)[1];
+    Vue.component(name, components(key).default);
+  });
+}
+
+const plugin = {
+  version: require('../package.json').version,
+  install,
+};
+
+export default plugin;
+
+// Auto-install
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+  GlobalVue = window['Vue'];
+} else if (typeof global !== 'undefined') {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
---
+++ vue.config.js
@@ -0,0 +1,60 @@
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+function resolve(dir) {
+  return path.join(__dirname, dir);
+}
+
+module.exports = {
+  lintOnSave: true,
+  configureWebpack: {
+    entry: {
+      app: path.resolve(__dirname, "./examples/main.ts")
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set("@", resolve("src"))
+      .set("@img", resolve("src/assets/images"))
+      .set("@css", resolve("src/assets/css"))
+      .set("@scss", resolve("src/assets/scss"))
+
+    config.module
+      .rule('js')
+      .include
+      .add(path.resolve(__dirname, './examples'))
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.compilerOptions.preserveWhitespace = true
+        return options
+      })
+  },
+  productionSourceMap: false,
+  css: {
+    extract: true,
+    sourceMap: false,
+    loaderOptions: {
+      sass: {
+        data: `
+          @import "@/assets/scss/theme.scss";
+        `
+      }
+    },
+    modules: false
+  },
+  parallel: require("os").cpus().length > 1,
+  devServer: {
+    port: 1001,
+    open: true,
+    compress: false,
+    overlay: {
+      warnings: true,
+      errors: true
+    }
+  }
+};
