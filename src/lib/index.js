5dae689c3823108ccc1ed5a2d70da9d207502f49
luchx <lucx01@mingyuanyun.com>
2020-07-21 20:43:14 +0800

    删除文件

--- src/lib/index.js
+++
@@ -1,9 +0,0 @@
-import lcxModal from './lcxModal.vue' // 导入组件
-
-lcxModal.install = function (Vue) {
-  Vue.component('lcxModal', lcxModal)
-  if (typeof window !== 'undefined' && window.Vue) {
-    window.Vue.use(lcxModal);
-  }
-}
-export default lcxModal;
