675461bab3a8bbd7d12e7496b2d1ee2e6390815b
luchx <luchx1993@gmail.com>
2020-07-27 13:23:47 +0800

    feat: 添加 Tips 组件

--- src/components/tips-provider/index.jsx
+++ src/components/tips-provider/index.jsx
@@ -7,12 +7,17 @@ import { isType } from "@/utils/index";
  * @desc 全局挂载 tips 组件, 必须搭配 TipsProvider 使用
  * @example
  *
- * modifiers?: string 支持显示方向 => ["top", "right", "bottom", "left"]
+ * modifiers?: string | object 支持显示方向 => ["top", "right", "bottom", "left"]
  * message!: string 展示内容
  *
  * ```vue
- * 
+ *
  * Vue.use(TipsProvider);
+ *
+ * 支持直接以当前 target 对象传入，只支持在指令中使用
+ * <tips-provider>
+ *    <div v-tips>挂载节点（直接抓取 dom 节点展示）</div>
+ * </tips-provider>
  * 
  * 通过指令调用、模板中推荐用法
  * <tips-provider>
@@ -21,7 +26,7 @@ import { isType } from "@/utils/index";
  *    <div v-tips.bottom="下侧展示">挂载节点</div>
  *    <div v-tips.left="左侧展示">挂载节点</div>
  * </tips-provider>
- * 
+ *
  * 以属性名调用、jsx 中推荐用法
  * <tips-provider>
  *    <div data-tips="上侧展示" data-position="top">挂载节点</div>
@@ -29,19 +34,52 @@ import { isType } from "@/utils/index";
  *    <div data-tips="下侧展示" data-position="bottom">挂载节点</div>
  *    <div data-tips="左侧展示" data-position="left">挂载节点</div>
  * </tips-provider>
+ * 
+ * jsx 中使用指令调用
+ * <tips-provider>
+ *    // 以下指令将返回 string 类型
+ *    <div {...{
+        directives: [
+          {
+            name: "tips",
+            value: "左侧出现",
+            modifiers: "left",
+          },
+        ],
+      }}>挂载节点</div>
+
+      // 以下指令将返回 object => v-tips.right="右侧出现"
+ *    <div {...{
+        directives: [
+          {
+            name: "tips",
+            value: "右侧出现",
+            modifiers: {
+              right: true
+            }
+          },
+        ],
+      }}>挂载节点</div>
+
+      // 以下指令将直接使用挂载的 vnode => v-tips
+ *    <div {...{
+        directives: [
+          {
+            name: "tips",
+          },
+        ],
+      }}>挂载节点（直接抓取 dom 节点展示）</div>
+ * </tips-provider>
  * ```
  */
 
-let TipsEl = null;
+//  存储 Tips 对象实例
+let TipsInstance = null;
+// 定义一个 Set 存储 dom
 const tipsEleQuene = new Set();
 
 const TipsProvider = {
   name: "TipsProvider",
-  data() {
-    return {
-      el: null,
-    };
-  },
   mounted() {
     on(this.$el, "mousemove", this.handleEnter);
   },
@@ -51,31 +89,47 @@ const TipsProvider = {
   methods: {
     handleEnter(event) {
       const target = event.target;
-      const rect = target.getBoundingClientRect();
-      TipsEl.close();
-      if (tipsEleQuene.has(target)) {
-        const modifiers =
-          isType(target.__TipsModifiers__) === "string"
-            ? target.__TipsModifiers__
-            : Object.keys(target.__TipsModifiers__ || {}).join(",");
+      const targetRect = target.getBoundingClientRect();
+      TipsInstance.close();
+      const activeDom = [...tipsEleQuene].find((item) => item.contains(target));
 
-        TipsEl.open({
-          title: target.__TipsValue__,
-          elRect: rect,
+      // 通过指令挂载的对象
+      if (activeDom) {
+        const modifiers =
+          isType(activeDom.__TipsModifiers__) === "string"
+            ? activeDom.__TipsModifiers__
+            : Object.keys(activeDom.__TipsModifiers__ || {}).join(",");
+
+        // 当前没有传值，则直接使用 vnode 节点挂载
+        const title = activeDom.__TipsValue__ || activeDom.__TipsVNode__;
+
+        TipsInstance.open({
+          title,
+          elRect: targetRect,
           position: modifiers,
         });
         return;
       }
-      const hasTips = target.hasAttribute("data-tips");
-      if (hasTips) {
-        const dataTips = target.getAttribute("data-tips");
-        const position = target.getAttribute("data-position");
-        TipsEl.open({
-          title: dataTips,
-          elRect: rect,
-          position,
-        });
+
+      // 通过 dom 属性挂载的对象，注意此处返回的是一个类数组
+      const selectTips = this.$el.querySelectorAll("[data-tips]") || [];
+      const selectTarget = [...selectTips].find((item) =>
+        item.contains(target)
+      );
+
+      if (!selectTarget) {
+        return;
       }
+      // 获取dom 节点的 data-tips 属性
+      const dataTips = selectTarget.getAttribute("data-tips");
+      // 获取dom 节点的 data-position 属性
+      const position = selectTarget.getAttribute("data-position");
+
+      TipsInstance.open({
+        title: dataTips,
+        elRect: targetRect,
+        position,
+      });
     },
   },
   render() {
@@ -86,16 +140,17 @@ const TipsProvider = {
 export default {
   install(Vue) {
     let TipsVNode = Vue.extend(TipsComponent);
-    if (!TipsEl) {
-      TipsEl = new TipsVNode().$mount();
-      document.body.appendChild(TipsEl.$el);
+    if (!TipsInstance) {
+      TipsInstance = new TipsVNode().$mount();
+      document.body.appendChild(TipsInstance.$el);
     }
 
     Vue.component("TipsProvider", TipsProvider);
     Vue.directive("tips", {
-      bind(el, binding) {
+      bind(el, binding, vNode) {
         el.__TipsValue__ = binding.value;
         el.__TipsModifiers__ = binding.modifiers;
+        el.__TipsVNode__ = vNode;
         tipsEleQuene.add(el);
       },
       unbind(el) {
